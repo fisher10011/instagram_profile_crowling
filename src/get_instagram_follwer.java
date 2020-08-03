e88af0f2254fbde259351f42c2d8035e7856148e
fisher10011 <66191152+fisher10011@users.noreply.github.com>
2020-08-03 11:45:34 +0900

    Update get_instagram_follwer.java

--- src/get_instagram_follwer.java
+++ src/get_instagram_follwer.java
@@ -12,14 +12,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class get_instagram_follwer {
 	
-	    //WebDriver
 	    private WebDriver driver;
 	    private WebElement element;
-	    //Properties
 	    public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
 	    public static final String WEB_DRIVER_PATH = "chromedriver.exe";
 	    
-	    //크롤링 할 URL
+	    
 	    private String base_url;
 	    
 	    public get_instagram_follwer() {
@@ -38,7 +36,6 @@ public class get_instagram_follwer {
 	   
 	    public void get_follwer() {
 	        try {
-	            //get page (= 브라우저에서 url을 주소창에 넣은 후 request 한 것과 같다)
 	            driver.get(base_url);
 	            
 	            //System.out.println(driver.getPageSource());
